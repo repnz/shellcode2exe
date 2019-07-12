de247ef6a8394a2afd169ef6524b7c0039fd6d1a
repnz <repnzb@gmail.com>
2019-07-12 16:33:00 +0300

    Make some changes in the batch file

--- shellcode2exe.bat
+++ shellcode2exe.bat
@@ -1,15 +1,17 @@
 @echo off
 @if "%1"=="" goto help
+@if "%2"=="" goto help
+@if "%3"=="" goto help
 
 @echo Global Start > shellcode.asm
 @echo SECTION 'foo' write, execute,read >> shellcode.asm
 @echo Start:       >> shellcode.asm
 @echo incbin "%2"  >> shellcode.asm
 @yasm.exe -f win%1 -o shellcode.obj shellcode.asm
-@golink /ni /entry Start shellcode.obj
+@golink /ni /fo %3 /entry Start shellcode.obj
 @del shellcode.asm
 @del shellcode.obj
-@dir shellcode.exe
+@dir %3*
 
 @goto exit
 
@@ -18,6 +20,6 @@
 @echo Required Arguments:
 @echo - architecture: 32 or 64 (depending on the shellcode)
 @echo - shellcode blob file name
-
+@echo - Output executable file name
 @:exit
 echo.
